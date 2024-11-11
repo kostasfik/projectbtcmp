3016ee22d8865e3032d3e3b865e72d4c4ac937b8
kostas <kostasfikatass@gmail.com>
2024-11-11 15:50:16 +0200

    till get and post

---
+++ src/main/java/dev/ctrlspace/bootcamp2410/kostas/bootcamp2410kostas/Bootcamp2410KostasApplication.java
@@ -0,0 +1,13 @@
+package dev.ctrlspace.bootcamp2410.kostas.bootcamp2410kostas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp2410KostasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootcamp2410KostasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ctrlspace/bootcamp2410/kostas/bootcamp2410kostas/Bootcamp2410KostasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ctrlspace.bootcamp2410.kostas.bootcamp2410kostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcamp2410KostasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
